56cfe5ee9060984b9ea6a37fbea6825842984b9f
Christopher Davis <31138928+cdavis1456@users.noreply.github.com>
2017-11-06 10:28:26 -0600

    Add files via upload

---
+++ DummyData.js
@@ -0,0 +1,45 @@
+var audits = [{
+    ClientID: 1, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+    AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+    DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+}, {
+    ClientID: 2, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+    AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+    DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+    },
+{
+    ClientID: 3, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+    AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+    DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+},
+    {
+        ClientID: 4, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+        AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+        DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+},
+    {
+        ClientID: 5, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+        AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+        DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+    }, {
+        ClientID: 6, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+        AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+        DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+    }, {
+        ClientID: 7, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+        AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+        DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+    }, {
+        ClientID: 8, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+        AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+        DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+    }, {
+        ClientID: 9, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+        AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+        DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+    }, {
+        ClientID: 10, ClientName: "John Doe", ClientNumber: 1527, MatterNumber: "123-456", DateSubmitted: "10/18/2017", BillingAttorney: "Jim Doe",
+        AuditPeriodEndDate: "10/18/2017", DateSentToFinance: "10/18/2017", DateSentToBillingAttorney: "10/18/2017",
+        DateSentToCommitteeForReview: "10/18/2017", Status: "Approved", RequestForm: "link", Attachments: "link"
+    }
+];
